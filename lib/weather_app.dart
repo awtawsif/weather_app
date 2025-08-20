9b6c5f3358aa177fbf27ce85df044da6c1dab170
awtawsif <tawsif7492@gmail.com>
2025-08-20 18:25:31 +0600

    finish main card widget

--- lib/weather_app.dart
+++ lib/weather_app.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class WeatherApp extends StatefulWidget {
@@ -12,6 +14,62 @@ class WeatherApp extends StatefulWidget {
 class _WeatherApp extends State {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Text("Hello World"));
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Weather App"),
+        centerTitle: true,
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.refresh))],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                elevation: 20,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        children: [
+                          Text(
+                            "98.7°F",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 40,
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          Icon(Icons.cloud, size: 64),
+                          SizedBox(height: 10),
+                          Text(
+                            "Rain",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Placeholder(fallbackHeight: 150),
+            SizedBox(height: 20),
+            Placeholder(fallbackHeight: 150),
+          ],
+        ),
+      ),
+    );
   }
 }
