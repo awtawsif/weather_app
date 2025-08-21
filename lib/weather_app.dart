cf604ed7a0cba98bfb6f066a9e29f11f2a9bf7ee
awtawsif <tawsif7492@gmail.com>
2025-08-21 11:36:41 +0600

    enable const suggestion, add const where necessary, modularize hourlyforecast into one class

--- lib/weather_app.dart
+++ lib/weather_app.dart
@@ -18,7 +18,9 @@ class _WeatherApp extends State {
       appBar: AppBar(
         title: const Text("Weather App"),
         centerTitle: true,
-        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.refresh))],
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.refresh)),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16),
@@ -36,8 +38,8 @@ class _WeatherApp extends State {
                   borderRadius: BorderRadius.circular(16),
                   child: BackdropFilter(
                     filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                    child: Padding(
-                      padding: const EdgeInsets.all(16),
+                    child: const Padding(
+                      padding: EdgeInsets.all(16),
                       child: Column(
                         children: [
                           Text(
@@ -64,149 +66,58 @@ class _WeatherApp extends State {
                 ),
               ),
             ),
-            SizedBox(height: 20),
-            Text(
+            const SizedBox(height: 20),
+            const Text(
               "Weather Forecast",
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
             ),
-            SizedBox(height: 16),
-            SingleChildScrollView(
+            const SizedBox(height: 16),
+            const SingleChildScrollView(
               scrollDirection: Axis.horizontal,
-
               child: Row(
                 children: [
-                  Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadiusGeometry.circular(16),
-                    ),
-                    elevation: 10,
-                    child: Container(
-                      width: 110,
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        children: [
-                          Text(
-                            "9:00",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                            ),
-                          ),
-                          SizedBox(height: 8),
-                          Icon(Icons.cloud, size: 32),
-                          SizedBox(height: 8),
-                          Text("100.23"),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadiusGeometry.circular(16),
-                    ),
-                    elevation: 10,
-                    child: Container(
-                      width: 110,
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        children: [
-                          Text(
-                            "9:00",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                            ),
-                          ),
-                          SizedBox(height: 8),
-                          Icon(Icons.cloud, size: 32),
-                          SizedBox(height: 8),
-                          Text("100.23"),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadiusGeometry.circular(16),
-                    ),
-                    elevation: 10,
-                    child: Container(
-                      width: 110,
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        children: [
-                          Text(
-                            "9:00",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                            ),
-                          ),
-                          SizedBox(height: 8),
-                          Icon(Icons.cloud, size: 32),
-                          SizedBox(height: 8),
-                          Text("100.23"),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadiusGeometry.circular(16),
-                    ),
-                    elevation: 10,
-                    child: Container(
-                      width: 110,
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        children: [
-                          Text(
-                            "9:00",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                            ),
-                          ),
-                          SizedBox(height: 8),
-                          Icon(Icons.cloud, size: 32),
-                          SizedBox(height: 8),
-                          Text("100.23"),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadiusGeometry.circular(16),
-                    ),
-                    elevation: 10,
-                    child: Container(
-                      width: 110,
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        children: [
-                          Text(
-                            "9:00",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                            ),
-                          ),
-                          SizedBox(height: 8),
-                          Icon(Icons.cloud, size: 32),
-                          SizedBox(height: 8),
-                          Text("100.23"),
-                        ],
-                      ),
-                    ),
-                  ),
+                  HourlyForecast(),
+                  HourlyForecast(),
+                  HourlyForecast(),
+                  HourlyForecast(),
+                  HourlyForecast(),
                 ],
               ),
             ),
-            SizedBox(height: 20),
-            Placeholder(fallbackHeight: 150),
-            SizedBox(height: 20),
-            Placeholder(fallbackHeight: 150),
+            const SizedBox(height: 20),
+            const Placeholder(fallbackHeight: 150),
+            const SizedBox(height: 20),
+            const Placeholder(fallbackHeight: 150),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HourlyForecast extends StatelessWidget {
+  const HourlyForecast({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadiusGeometry.circular(16),
+      ),
+      elevation: 10,
+      child: Container(
+        width: 110,
+        padding: const EdgeInsets.all(8.0),
+        child: const Column(
+          children: [
+            Text(
+              "9:00",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+            SizedBox(height: 8),
+            Icon(Icons.cloud, size: 32),
+            SizedBox(height: 8),
+            Text("100.23"),
           ],
         ),
       ),
